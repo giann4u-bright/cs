fc2d82141cb5dcb0e6b52e3aaa267240a5e35513
Jihyun <giann4u@gmail.com>
2026-06-03 20:07:57 +0900

    Add API handler for POST requests to Notion

---
+++ api/api.submit.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const response = await fetch('https://api.notion.com/v1/pages', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28'
+    },
+    body: JSON.stringify(req.body)
+  });
+
+  const data = await response.json();
+  res.status(response.status).json(data);
+}
