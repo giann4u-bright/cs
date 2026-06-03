0149eae3622f17f47b78a505ec9ad10bcb39d96a
Jihyun <giann4u@gmail.com>
2026-06-03 20:37:13 +0900

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -3,6 +3,11 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
+  const body = req.body;
+
+  // DB_ID를 환경변수에서 받아오기
+  body.parent = { database_id: process.env.NOTION_DB_ID };
+
   const response = await fetch('https://api.notion.com/v1/pages', {
     method: 'POST',
     headers: {
@@ -10,7 +15,7 @@ export default async function handler(req, res) {
       'Content-Type': 'application/json',
       'Notion-Version': '2022-06-28'
     },
-    body: JSON.stringify(req.body)
+    body: JSON.stringify(body)
   });
 
   const data = await response.json();
